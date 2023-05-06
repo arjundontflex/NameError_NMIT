d5d116b19d6d85e0c1e8767437c741392fe44425
princeo741 <113790710+princeo741@users.noreply.github.com>
2023-05-06 16:48:16 +0530

    Upaded GUI app.py

--- GUI/app.py
+++ GUI/app.py
@@ -19,16 +19,20 @@ background_label = tk.Label(root, image=background_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Create a welcome message label
-welcome_label = tk.Label(root, text="Welcome to My App!", font=("Arial", 20))
+welcome_label = tk.Label(root, text="Welcome to My App!", font=("Arial", 20), bg='black', fg='white')
 welcome_label.pack(pady=20)
 
 # Create the "revision" button
-revision_button = tk.Button(root, text="Revision", font=("Arial", 16), command=lambda: os.system("python revision.py"), bd=0, bg="#6ab04c", fg="white", activebackground="#82c983", activeforeground="white", padx=20, pady=10, borderwidth=0, highlightthickness=0, relief="flat", cursor="hand2")
+revision_button = tk.Button(root, text="Revision", font=("Arial", 16), command=lambda: os.system("python revision.py"), bd=0, bg="blue", fg="white", activebackground="#82c983", activeforeground="white", padx=20, pady=10, borderwidth=0, highlightthickness=0, relief="flat", cursor="hand2")
 revision_button.place(relx=0.3, rely=0.4, anchor="center")
 
 # Create the "Prepare for exam" button
 exam_button = tk.Button(root, text="Prepare for exam", font=("Arial", 16), command=lambda: os.system("python exam.py"), bd=0, bg="#ea7317", fg="white", activebackground="#ff902e", activeforeground="white", padx=20, pady=10, borderwidth=0, highlightthickness=0, relief="flat", cursor="hand2")
 exam_button.place(relx=0.7, rely=0.4, anchor="center")
+
+# Create the "exit" button
+exit_button = tk.Button(root, text="Exit", font=("Arial", 16), command=root.destroy, bd=0, bg="red", fg="white", activebackground="#333333", activeforeground="white", padx=20, pady=10, borderwidth=0, highlightthickness=0, relief="flat", cursor="hand2")
+exit_button.place(relx=0.5, rely=0.8, anchor="center")
 
 # Start the main loop
 root.mainloop()
